257611d54c266520c6d34c169aeb5f6fec85d56b
李通洲 <carter.li@eoitek.com>
2021-08-17 21:40:42 +0800

    init code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # websocketstream-polyfill
+
 WebSocketStream Polyfill
+
+[WebSocket](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket) with [Streams API](https://developer.mozilla.org/en-US/docs/Web/API/Streams_API)
+
+## Explainer
+
+https://github.com/ricea/websocketstream-explainer
+
+## Detailed Design
+
+https://docs.google.com/document/d/1La1ehXw76HP6n1uUeks-WJGFgAnpX2tCjKts7QFJ57Y/edit#
+
+## LICENSE
+
+MIT
---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * [WebSocket](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket) with [Streams API](https://developer.mozilla.org/en-US/docs/Web/API/Streams_API)
+ *
+ * @see https://web.dev/websocketstream/
+ */
+export class WebSocketStream {
+    url;
+    connection;
+    closed;
+    close;
+    constructor(url, options = {}) {
+        if (options.signal?.aborted) {
+            throw new DOMException('This operation was aborted', 'AbortError');
+        }
+        this.url = url;
+        const ws = new WebSocket(url, options.protocols ?? []);
+        const closeWithInfo = ({ code, reason } = {}) => ws.close(code, reason);
+        this.connection = new Promise((resolve, reject) => {
+            ws.onopen = () => {
+                resolve({
+                    readable: new ReadableStream({
+                        start(controller) {
+                            ws.onmessage = ({ data }) => controller.enqueue(data);
+                            ws.onerror = e => controller.error(e);
+                        },
+                        cancel: closeWithInfo,
+                    }),
+                    writable: new WritableStream({
+                        write(chunk) { ws.send(chunk); },
+                        abort() { ws.close(); },
+                        close: closeWithInfo,
+                    }),
+                    protocol: ws.protocol,
+                    extensions: ws.extensions,
+                });
+                ws.removeEventListener('error', reject);
+            };
+            ws.addEventListener('error', reject);
+        });
+        this.closed = new Promise((resolve, reject) => {
+            ws.onclose = ({ code, reason }) => {
+                resolve({ code, reason });
+                ws.removeEventListener('error', reject);
+            };
+            ws.addEventListener('error', reject);
+        });
+        if (options.signal) {
+            options.signal.onabort = () => ws.close();
+        }
+        this.close = closeWithInfo;
+    }
+}
+//# sourceMappingURL=index.js.map
